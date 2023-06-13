4b1261fee2bb8601958bd16c67ae216d99e4442b
Nikhil <nikhilkumarsingh7174@gmail.com>
2023-06-13 21:22:14 +0530

    Added env variables

--- Backend/db.js
+++ Backend/db.js
@@ -1,5 +1,3 @@
-// import cors from "cors";
-// import * as dotenv from "dotenv";
 require("dotenv").config();
 const mongoose = require("mongoose");
 const mongoURI = process.env.CONNECTION_URL;
@@ -10,16 +8,3 @@ const connectToMongo = () => {
   });
 };
 module.exports = connectToMongo;
-
-// const CONNECTION_URL = process.env.CONNECTION_URL;
-
-// const PORT = process.env.PORT || 5000;
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://nikhilkumarsingh7174:gRpdGk4QbNc5e@cluster01.drckdar.mongodb.net/?retryWrites=true&w=majority"
-//   )
-//   .then(() =>
-//     app.listen(PORT, () => console.log(`Server running on port:${PORT}`))
-//   )
-//   .catch((error) => console.log("some error", error));
